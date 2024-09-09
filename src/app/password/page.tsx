ab477b5173b5b5953024c8a50ee2c995dffd98fb
mohamed youssef <m.youssef315@yahoo.com>
2024-09-09 04:19:04 +0300

    feat: edit timer

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -11,7 +11,7 @@ export default async function Password() {
     <main className="min-h-[calc(100vh-74px)] bg-[url('/info.jpg')] bg-cover bg-center bg-no-repeat flex">
       <Suspense fallback={<LoadingSpinner />}>
         <div className="w-11/12 flex flex-col items-center gap-4 p-6 px-6 mx-auto my-5 bg-white rounded-2xl shadow-xl">
-          <Timer initialSeconds={5} />
+          <Timer initialSeconds={30} />
           <NameCard />
           <div className="w-full p-4 text-center flex flex-wrap gap-2 items-center justify-around">
             <PasswordScore team={1} />
