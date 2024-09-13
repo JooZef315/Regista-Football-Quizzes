13960bb7e6d8c0a9b3f48e95da4a539fb4e08ba5
mohamed youssef <m.youssef315@yahoo.com>
2024-09-13 21:09:19 +0300

    feature(Password): edit timer typeo

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -10,7 +10,7 @@ export default async function Password() {
     <main className="min-h-[calc(100vh-74px)] bg-[url('/info.jpg')] bg-cover bg-center bg-no-repeat flex">
       <Suspense fallback={<LoadingSpinner />}>
         <div className="relative w-11/12 flex flex-col justify-center items-center gap-4 p-6 px-6 mx-auto my-5 bg-white rounded-2xl shadow-xl">
-          <Timer initialSeconds={10} />
+          <Timer initialSeconds={30} />
           <NameCard />
           <PasswordUtil />
           <div className="w-full p-4 text-center flex flex-wrap gap-2 items-center justify-around">
